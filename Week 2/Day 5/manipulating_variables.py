ec407e4103f4c193cf8ca27c2f2752c1928e5d4e
Nirjana Shrestha <167171103+nirjanashrestha4@users.noreply.github.com>
2024-06-30 17:10:21 +1000

    file is added

---
+++ Week 2/Day 5/manipulating_variables.py
@@ -0,0 +1,31 @@
+# This is a Python comment in my first Python app.
+"""This is a multiline comment in Python This type of comment is sometimes called a docstring. A docstring starts with three double-quotation marks, and also ends with three double quatation marks. """
+"Mary's dog said \"woof\"."
+"The old pond\nA frog jumped in, \nKerplunk!"
+
+# This variable contains an integer.
+quantity = 10
+# This variable contains a float.
+unit_price = 1.99
+# This variable contains the result of multiplying quantity times unit price
+extended_price = quantity * unit_price
+# Show the results
+print(extended_price)
+
+
+# This is a Python comment in my first Python app.
+# This variable contains an integer.
+quantity = 14
+# This variable contains a float
+unit_price = 25.99
+# This variable contains the result of multiplying quantity times unit price
+extended_price = quantity * unit_price
+# Show some resluts on the screen.
+print(extended_price)
+
+x = 13
+if x == 0:
+    print("x is zero")
+else:
+    print(" x is ", x)
+print("All done")
